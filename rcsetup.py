bb822bf98098eb1b92a4dfb40dd1af98cb5725a8
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-01-27 14:15:59 -0800

    Add rcsetup.py to setup parameters.
    rcsetup.py will contain code to allow loading of parameters from rc files, and
    will set default values when parameters are not provided.

---
+++ rcsetup.py
@@ -0,0 +1,7 @@
+"""
+Sets up parameters for a model run. Used to read in settings from any provided 
+rc file, and set default values for any parameters that are not provided in the 
+rc file.
+"""
+
+
