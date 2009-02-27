cf2c31030969c3a7c33ba25c3df2a4627b55c6b4
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-02-26 17:53:29 -0800

    Update agents variable names for style.

--- agents.py
+++ agents.py
@@ -151,26 +151,26 @@ class Household(Agent_set):
     "Represents a single household agent"
     def __init__(self, ID=None, initial_agent=False):
         Agent_set.__init__(self, HIDGen, ID, initial_agent)
-        self._anyNonWoodFuel = boolean_choice()
-        self._OwnHousePlot = boolean_choice()
-        self._OwnLand = boolean_choice()
-        self._RentedOutLand = boolean_choice()
+        self._any_non_wood_fuel = boolean_choice()
+        self._own_house_plot = boolean_choice()
+        self._own_land = boolean_choice()
+        self._rented_out_land = boolean_choice()
 
     def any_non_wood_fuel(self):
         "Boolean for whether household uses any non-wood fuel"
-        return self._anyNonWoodFuel
+        return self._any_non_wood_fuel
 
     def own_house_plot(self):
         "Boolean for whether household owns the plot of land on which it resides"
-        return self._OwnHousePlot
+        return self._own_house_plot
 
     def own_any_land(self):
         "Boolean for whether household owns any land"
-        return self._OwnLand
+        return self._own_land
 
     def rented_out_land(self):
         "Boolean for whether household rented out any of its land"
-        return self._RentedOutLand
+        return self._rented_out_land
 
 NIDGen = IDGenerator()
 class Neighborhood(Agent_set):
