a52ec02acfb8d0b2b6cc9cb4ab2e86c7be9ab77d
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-08-23 14:32:43 -0700

    Add GPL license, and licensing info to agents.py.

--- agents.py
+++ agents.py
@@ -1,7 +1,23 @@
-"""
-Part of Chitwan Valley agent-based model.
+# Copyright © 2009 Alex Zvoleff
+#
+# This file is part of the ChitwanABM agent-based model.
+# 
+# ChitwanABM is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# 
+# ChitwanABM is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# ChitwanABM.  If not, see <http://www.gnu.org/licenses/>.
 
-Class for person, household, neighborhood, and region agents.
+"""
+Contains the classes for Person, Household, Neighborhood, and Region agents. 
+Person agents are subclasses of the Agent class, while Household, Neighborhood, 
+and Region agents are all subclasses of the Agent_set object.
 
 Alex Zvoleff, azvoleff@mail.sdsu.edu
 """
