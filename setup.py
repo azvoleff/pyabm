b3d323ec5d1ccc8cced6864917271ec7a0779cb2
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-09-04 08:51:08 -0700

    Work on top-level docs.

--- setup.py
+++ setup.py
@@ -18,27 +18,10 @@ setup(
     author = "Alex Zvoleff",
     author_email = "azvoleff@mail.sdsu.edu",
     description = "Agent-based modeling toolkit",
-    license = "GPL v3 or later",
     keywords = "agent-based modeling ABM simulation model",
+    license = "GPL v3 or later",
     url = "http://rohan.sdsu.edu/~zvoleff/research/PyABM.php",   # project home page, if any
-    long_description = """
-pyabm is an agent-based modeling toolkit written to simplify coding and 
-running agent-based models in the Python programming language. pyabm 
-includes basic classes for creation of agents, has tools to output model 
-results (as text files, plots or ESRI shapefiles), has a parameter handling 
-system to ease the process of model testing and validation, and has 
-utilities to assist in running multiple scenarios with varying model 
-parameters.
-
-Construction of pyabm is supported as part of an ongoing National Science 
-Foundation Partnerships for International Research and Education (NSF PIRE) 
-project `(grant OISE 0729709) <http://pire.psc.isr.umich.edu>`_
-Development of pyabm is ongoing, and the documentation is still a work in 
-progress.
-
-See the `pyabm website <http://rohan.sdsu.edu/~zvoleff/research/PyABM.php>`_ 
-for more information, past releases, publications, and recent presentations.
-""",
+    long_description = open('README.rst').readlines()[6:],
 	classifiers = [
         "Development Status :: 3 - Alpha",
         "Natural Language :: English",
