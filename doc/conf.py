68b126632cc87dabb7d9192c5240ab396a8c5375
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-09-03 20:53:53 -0700

    Doc options.

--- doc/conf.py
+++ doc/conf.py
@@ -84,7 +84,7 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
+modindex_common_prefix = ['pyabm']
 
 
 # -- Options for HTML output ---------------------------------------------------
@@ -106,7 +106,7 @@ html_theme = 'default'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'pyabm Documentation'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -124,7 +124,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
